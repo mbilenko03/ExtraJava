35b494a7a830a166be4f9522c99f55d728af4a2a
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-14 09:19:35 -0500

    started chapter 14

---
+++ src/gui/JFrame1.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+
+public class JFrame1
+{
+
+	public static void main(String[] args)
+	{
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		JFrame aFrame = new JFrame("First frame");
+		aFrame.setSize(250,100);
+		aFrame.setVisible(true);		
+	}
+
+}
