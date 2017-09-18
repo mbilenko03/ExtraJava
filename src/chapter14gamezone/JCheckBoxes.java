a86bb51053133c22a61606d309dc6c56428930c5
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-18 10:02:35 -0500

    update

--- src/chapter14gamezone/JCheckBoxes.java
+++ src/chapter14gamezone/JCheckBoxes.java
@@ -4,8 +4,6 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -21,7 +19,6 @@ public class JCheckBoxes extends JFrame implements ActionListener
 			"Which of the following have the quail as their state bird?",
 			"Which of the following have the largest area?"
 			};
-
 	JLabel questionLabel = new JLabel(questions[1]);
 
 	//info
