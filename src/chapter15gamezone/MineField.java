592ed39dfe5247f90c6dad44af1cd3aa01eb92c8
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-22 09:25:30 -0500

    reset

--- src/chapter15gamezone/MineField.java
+++ src/chapter15gamezone/MineField.java
@@ -5,97 +5,26 @@ import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
 import javax.swing.event.MouseInputListener;
 
+import sun.applet.Main;
 
-public class MineField extends JFrame implements MouseInputListener 
-{
-	JLabel message = new JLabel(); //North Congratz Message
-	Container con = null;
-	GridLayout field = new GridLayout(4,5);
-	JPanel pane = new JPanel(field);
-	JPanel[] panel = new JPanel[20];
-	
+
+public class MineField extends JFrame
+{	
 	public MineField() 
 	{
-	    con = this.getContentPane();
-	    con.setLayout(new BorderLayout());
-	    this.message.setText("Test");
-		con.add(message, BorderLayout.NORTH);
-		
-		for(int i = 0; i < 20; ++i)
-		{
-			panel[i] = new JPanel();
-			con.add(panel[i]);
-			panel[i].setBackground(Color.BLUE);
-			panel[i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		}
-		
-		addMouseListener(this);
 		
 	}
 	
-	public static void main(String[] args)
-	{
-	      MineField f = new MineField();
-	      f.setSize(300, 300);
-	      f.setVisible(true);
-	}
 	
-
-	
-	@Override
-	public void mouseClicked(MouseEvent e) 
+	public static void main(String[] args) 
 	{
 		
 	}
-
-	@Override
-	public void mouseEntered(MouseEvent arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent arg0)
-	{
-		// TODO Auto-generated method stub
-		
-	} 
-
-
-
 }
