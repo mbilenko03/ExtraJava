3f4e020099de828b948dc76f43a7392157cb200a
Michael Bilenko <mbilenko03@users.noreply.github.com>
2017-09-30 19:13:00 -0500

    fixed numers

--- src/chapter15gamezone/MineSweeper.java
+++ src/chapter15gamezone/MineSweeper.java
@@ -7,9 +7,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,30 +18,30 @@ public class MineSweeper extends JFrame implements ActionListener
 {
 	JPanel mainPanel = new JPanel();
 	JPanel gamePanel = new JPanel();
-	
+
 	JLabel message = new JLabel();
 
 	final static int sizeHeigth = 4;
 	final static int sizeWidth = 5;
 	final static int numberOfBombs = 4;
-	
+
 	Font textFont = new Font("Comic Sans", Font.BOLD, 15);
-	
+
 	static JButton[] buttons = new JButton[sizeHeigth * sizeWidth];
-	
+
 	static List<Integer> bombLocations = new ArrayList<Integer>();
-	
-	public MineSweeper () 
+
+	public MineSweeper()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 500);
 		setVisible(true);
-		
+
 		message.setText("Mine Sweeper");
-		
+
 		gamePanel.setLayout(new GridLayout(sizeHeigth, sizeWidth));
 
-		for (int i = 0; i < (sizeHeigth * sizeWidth); i++) 
+		for (int i = 0; i < (sizeHeigth * sizeWidth); i++)
 		{
 			buttons[i] = new JButton();
 			gamePanel.add(buttons[i]);
@@ -51,60 +49,54 @@ public class MineSweeper extends JFrame implements ActionListener
 			buttons[i].setBackground(Color.WHITE);
 			buttons[i].setFont(textFont);
 		}
-		
+
 		mainPanel.setLayout(new BorderLayout());
 		mainPanel.add(message, BorderLayout.NORTH);
 		mainPanel.add(gamePanel, BorderLayout.CENTER);
 		add(mainPanel);
 
-		
-		for (int i = 0; i < numberOfBombs; i++) 
-		{	
+		for (int i = 0; i < numberOfBombs; i++)
+		{
 			bombLocations.add(getUniqueRand());
 			buttons[bombLocations.get(i)].setText("BOMB");
-			//prevent from bomb being created in same spot
+			// prevent from bomb being created in same spot
 		}
-		
-		for (int i = 0; i < (sizeHeigth * sizeWidth); i++) 
-		{
-			for (int j = 0; j < numberOfBombs; j++) 
-			{
-				if (i != bombLocations.get(j))
-					buttons[i].setText(Integer.toString(setBombValue(i)));
-				else
-					buttons[i].setText("BOMB");
 
+		for (int i = 0; i < (sizeHeigth * sizeWidth); i++)
+		{
+			if (!bombLocations.contains(i))
+			{
+				buttons[i].setText(Integer.toString(getBombValue(i)));
 			}
 		}
-		
 	}
-	
-	public static int getUniqueRand() 
+
+	public static int getUniqueRand()
 	{
-		int randomNumber = 1 + (int)(Math.random() * ((sizeHeigth * sizeWidth)-1));
-		
+		int randomNumber = 1 + (int) (Math.random() * ((sizeHeigth * sizeWidth) - 1));
+
 		for (int i = 0; i < bombLocations.size(); i++)
+		{
 			if (bombLocations.get(i) == randomNumber)
 				return getUniqueRand();
-				
+		}
+
 		return randomNumber;
 	}
-	
-	public static int setBombValue(int position)
+
+	public static int getBombValue(int position)
 	{
 		int totalBombCount = 0;
-		
-		int column =  sizeWidth % position - 1;
-		int row =  sizeHeigth % position - 1;
 
-		 
-		for (int i = 0; i < numberOfBombs; i++) 
+		int row = position % sizeWidth; // 100% correct
+		int column = position / sizeWidth;
+
+		for (int i = 0; i < numberOfBombs; i++)
 		{
 			int bombPosition = bombLocations.get(i);
-			int bombColumn = sizeWidth % bombPosition - 1;
-			int bombRow = sizeHeigth % bombPosition - 1;
+			int bombRow = bombPosition % sizeWidth;
+			int bombColumn = bombPosition / sizeWidth;
 
-			
 			if (bombRow == row + 1 && bombColumn == column)
 				totalBombCount++;
 			if (bombRow == row - 1 && bombColumn == column)
@@ -121,20 +113,19 @@ public class MineSweeper extends JFrame implements ActionListener
 				totalBombCount++;
 			if (bombRow == row - 1 && bombColumn == column - 1)
 				totalBombCount++;
-			
-			//check row+1, row-1, 
-			//column+1, column-1, 
-			//row+1 column+1, row+1 column-1, 
-			//row-1 column+1, row-1 column-1 
+
+			// check row+1, row-1,
+			// column+1, column-1,
+			// row+1 column+1, row+1 column-1,
+			// row-1 column+1, row-1 column-1
 		}
 
-		
-		return totalBombCount;	
+		return totalBombCount;
 	}
 
 	public static void main(String[] args)
 	{
-	    MineSweeper frame = new MineSweeper();
+		MineSweeper frame = new MineSweeper();
 	}
 
 	@Override
@@ -146,7 +137,12 @@ public class MineSweeper extends JFrame implements ActionListener
 		// show values
 		// else if location is normal square
 		// make gray and show value
-		
+
+		for (int i = 0; i < (sizeHeigth * sizeWidth); i++)
+		{
+
+		}
+
 	}
 
 }
